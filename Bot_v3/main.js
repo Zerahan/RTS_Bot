f88c1c617521abb6df547f81db7d65f901c02641
James Isbell <Zerahan@users.noreply.github.com>
2023-09-07 20:53:36 -0700

    init

---
+++ Bot_v3/main.js
@@ -0,0 +1,33 @@
+var MiscPrototype = require('prototype.misc');
+var RoomPrototype = require('prototype.room');
+var RoomPositionPrototype = require('prototype.roomposition');
+var CreepPrototype = require('prototype.creep');
+var SpawnPrototype = require('prototype.structureSpawn');
+var BasicRole = require('role.basic');
+var ConstructionPlanner = require('planner.construction');
+var OutpostManager = require('manager.outpost');
+
+module.exports.loop = function () {
+    Game.debugMode = true;
+    var showGameTick = false;
+    var roomToObserve = "W9N1";
+    if(showGameTick){
+        var str = "Game tick: " + Game.time;
+        var hr = "";
+        for(var i = 0; i < str.length; i++){
+            hr = hr + "-";
+        }
+        console.log(hr);
+        console.log(str);
+    }
+    for(const k in Game.rooms){
+        const room = Game.rooms[k];
+        BasicRole.run(room);
+        ConstructionPlanner.run(room);
+        const observers = room.find(STRUCTURE_OBSERVER);
+        for(const o in observers){
+            o.observeRoom(roomToObserve);
+        }
+        OutpostManager.run(room);
+    }
+}
